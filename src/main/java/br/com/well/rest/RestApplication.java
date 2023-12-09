91084e0ccd36f7648f5e87362298003cb2563b4d
Wellington Ramos Nogueira <wellington.nogueira@gmail.com>
2023-12-08 21:07:12 -0300

    temporariamente excluindo DataSourceAutoConfiguration.class

--- src/main/java/br/com/well/rest/RestApplication.java
+++ src/main/java/br/com/well/rest/RestApplication.java
@@ -2,8 +2,9 @@ package br.com.well.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) //TODO remove this exclusion once database is needed.
 public class RestApplication {
 
 	public static void main(String[] args) {
